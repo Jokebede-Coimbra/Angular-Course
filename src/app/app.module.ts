6ec6b7bb6d9fc507c52d6481924318534d5b9aae
Coimbra <adsjkbd@gmail.com>
2020-12-05 13:32:42 -0300

    atualizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
 import { CourseListComponent } from './courses/course-list.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Erro404Component } from './error-404/error-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Erro404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+      path: '', redirectTo: 'courses', pathMatch: 'full'
+    },
+    {
+      path: 'courses', component: CourseListComponent
+    },
+    {
+      path: '**', component: Erro404Component
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
