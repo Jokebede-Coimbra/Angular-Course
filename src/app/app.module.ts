16aa62f48936bf2fc963e80aa57db8ba153d41d9
Coimbra <adsjkbd@gmail.com>
2020-12-08 12:18:38 -0300

    atualizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,31 @@
-import { CourseInfoComponent } from './courses/course-info.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { StarComponent } from './star/star.component';
-import { CourseListComponent } from './courses/course-list.component';
+import { CoreModule } from './core/core.module';
+
 import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { Erro404Component } from './error-404/error-404.component';
+
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    NavBarComponent,
-    Erro404Component,
-    CourseInfoComponent
-    
+    AppComponent, 
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
+    CoreModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
 
-      {
-        path: '**', component: Erro404Component
-      }
     ])
   ],
   providers: [],
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,25 @@
+import { RouterModule } from '@angular/router';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { NgModule } from '@angular/core';
+import { Erro404Component } from './component/error-404/error-404.component';
+
+@NgModule({
+    declarations: [
+        NavBarComponent,
+        Erro404Component
+    ],
+    imports: [
+        RouterModule.forChild([
+            {
+                path: '**', component: Erro404Component
+            }
+        ])
+    ],
+    exports: [
+        NavBarComponent
+    ]
+})
+
+export class CoreModule {
+
+}
